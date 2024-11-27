37d94bf14e7144da2ff542961d0a79a46190e14b
Rafael Teixeira <133879006+Rafaelvl2023@users.noreply.github.com>
2024-11-26 21:42:56 -0300

    fix: correcao-na-sintaxe

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -26,7 +26,7 @@ class AuthController extends Controller
         ]);
 
         if (Auth::attempt($request->only('email', 'password'))) {
-            $request->session()->regenerate(); // Regenera a sessão
+            $request->session()->regenerate();
             return redirect()->intended('/dashboard')->with('success', 'Login realizado com sucesso!');
         }
 
